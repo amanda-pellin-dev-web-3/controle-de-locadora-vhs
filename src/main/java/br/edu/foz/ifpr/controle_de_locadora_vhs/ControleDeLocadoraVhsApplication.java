0132c5c3fac275c601f16ee335a10f056a30f5b3
amanda.pp <amanda.pp@itaipuparquetec.org.br>
2025-07-09 08:16:22 -0300

    feat: initialize project

---
+++ src/main/java/br/edu/foz/ifpr/controle_de_locadora_vhs/ControleDeLocadoraVhsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.foz.ifpr.controle_de_locadora_vhs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeLocadoraVhsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeLocadoraVhsApplication.class, args);
+	}
+
+}
